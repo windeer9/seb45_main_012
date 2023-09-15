273fd10cc613b6bc1342f54a37bfb792f75fe202
matchaing <alswn202020@gmail.com>
2023-09-15 18:50:21 +0900

    refactor : navBar 열거형으로 수정

--- client/src/components/NavBar.jsx
+++ client/src/components/NavBar.jsx
@@ -9,76 +9,57 @@ const NavBar = () => {
   const dispatch = useDispatch();
   const navigate = useNavigate();
 
+  const BOARD_MENU = {
+    ALL: '전체 글 보기',
+    FREE: '자유 게시판',
+    AUTH: '인증 게시판',
+    ENV: '환경 정보 게시판',
+  };
+  
+  const MY_MENU = {
+    MY_PAGE: '내가 쓴 글',
+    MY_INFO: '내 정보',
+  };
+
   const handleMenuClick = (menuName) => {
     dispatch(setActiveMenu(menuName));
 
-    if (menuName === '전체 글 보기') {
-      navigate('/');
-      return;
-    }
-    else if (menuName === '자유 게시판') {
-      navigate('/free');
-      return;
-    }
-    else if (menuName === '인증 게시판') {
-      navigate('/auth');
-      return;
-    }
-    else if (menuName === '환경 정보 게시판') {
-      navigate('/env');
-      return;
-    }
-    else if (menuName === '내가 쓴 글') {
-      navigate('/mypage/main');
-      return;
-    }
-    else if (menuName === '내 정보') {
-      navigate('/mypage/info');
-      return;
+    const menuRoutes = {
+      [BOARD_MENU.ALL]: '/',
+      [BOARD_MENU.FREE]: '/free',
+      [BOARD_MENU.AUTH]: '/auth',
+      [BOARD_MENU.ENV]: '/env',
+      [MY_MENU.MY_PAGE]: '/mypage/main',
+      [MY_MENU.MY_INFO]: '/mypage/info',
+    };
+
+    if (menuRoutes[menuName]) {
+      navigate(menuRoutes[menuName]);
     }
   };
-
 
   return (
     <div className="navbar">
       <div className="text_menu_big">게시판</div>
-      <button
-        className={`menu_button ${activeMenu === '전체 글 보기' ? 'active' : ''}`}
-        onClick={() => handleMenuClick('전체 글 보기')}
-      >
-        전체 글 보기
-      </button>
-      <button
-        className={`menu_button ${activeMenu === '자유 게시판' ? 'active' : ''}`}
-        onClick={() => handleMenuClick('자유 게시판')}
-      >
-        자유 게시판
-      </button>
-      <button
-        className={`menu_button ${activeMenu === '인증 게시판' ? 'active' : ''}`}
-        onClick={() => handleMenuClick('인증 게시판')}
-      >
-        인증 게시판
-      </button>
-      <button
-        className={`menu_button ${activeMenu === '환경 정보 게시판' ? 'active' : ''}`}
-        onClick={() => handleMenuClick('환경 정보 게시판')}
-      >
-        환경 정보 게시판
-      </button>
+      {Object.values(BOARD_MENU).map((menuName) => (
+        <button
+          key={menuName}
+          className={`menu_button ${activeMenu === menuName ? 'active' : ''}`}
+          onClick={() => handleMenuClick(menuName)}
+        >
+          {menuName}
+        </button>
+      ))}
       <div className="text_menu_big">마이 페이지</div>
-      <button
-        className={`menu_button ${activeMenu === '내가 쓴 글' ? 'active' : ''}`}
-        onClick={() => handleMenuClick('내가 쓴 글')}
-      >
-        내가 쓴 글
-      </button>
-      <button
-        className={`menu_button ${activeMenu === '내 정보' ? 'active' : ''}`}
-        onClick={() => handleMenuClick('내 정보')}
-      >
-        내 정보
-      </button>
+      {Object.values(MY_MENU).map((menuName) => (
+        <button
+          key={menuName}
+          className={`menu_button ${activeMenu === menuName ? 'active' : ''}`}
+          onClick={() => handleMenuClick(menuName)}
+        >
+          {menuName}
+        </button>
+      ))}
     </div>
   );
 };
