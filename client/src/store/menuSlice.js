178d4bcd70ac5a6825b4fa82b0121f9afde11c6e
matchaing <alswn202020@gmail.com>
2023-09-15 18:54:35 +0900

    feat : 로컬스토리지에 현재 menu 상태 저장

--- client/src/store/menuSlice.js
+++ client/src/store/menuSlice.js
@@ -1,25 +1,34 @@
 import { createSlice } from '@reduxjs/toolkit';
 
-// 초기상태 비워뒀습니다
+// 초기 상태를 정의합니다.
 const initialState = {
   activeMenu: '전체 글 보기',
 };
 
-//슬라이스 생성
+// 로컬 스토리지에서 값을 읽어와서 초기 상태에 적용합니다.
+const storedActiveMenu = localStorage.getItem('activeMenu');
+if (storedActiveMenu) {
+  initialState.activeMenu = storedActiveMenu;
+}
+
+// 슬라이스 생성
 const menuSlice = createSlice({
   name: 'menu',
   initialState,
-  //리듀서 함수 정의
   reducers: {
     setActiveMenu: (state, action) => {
       state.activeMenu = action.payload;
+      // 메뉴가 변경될 때 로컬 스토리지에도 저장합니다.
+      localStorage.setItem('activeMenu', action.payload);
     },
   },
 });
 
 // 액션 생성자를 내보냅니다
 export const { setActiveMenu } = menuSlice.actions;
+
 // 선택자 함수를 내보냅니다 (현재 활성 메뉴를 선택하는 역할)
 export const selectActiveMenu = (state) => state.menu.activeMenu;
-// 리듀서 함수를 내보냅니다 (상태관리 수행하는 역할)
+
+// 리듀서 함수를 내보냅니다 (상태 관리 수행 역할)
 export default menuSlice.reducer;
