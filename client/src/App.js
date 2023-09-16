0fb25610243f065d5ed3a0e64f82b74a730c02f5
matchaing <alswn202020@gmail.com>
2023-09-17 07:39:15 +0900

    chore : 순서 변경

--- client/src/App.js
+++ client/src/App.js
@@ -1,19 +1,23 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import './App.css';
+
 import SignUpPage from './pages/SignUpPage.jsx';
 import Login from './pages/LoginPage.jsx';
+import EditerPage from './pages/EditerPage.jsx';
+
 import AllBoardPage from './pages/AllBoardPage.jsx';
 import FreeBoardPage from './pages/FreeBoardPage.jsx';
 import FreeDetailPage from './pages/FreeDetailPage.jsx';
-import MyPageInfo from './pages/MyPageInfo.jsx';
-
 import AuthBoardPage from './pages/AuthBoardPage.jsx';
 import AuthDetailPage from './pages/AuthDetailPage.jsx';
 import EnvBoardPage from './pages/EnvBoardPage.jsx';
+
 // import Header from './components/header.jsx';
 // import NavBar from './components/NavBar.jsx';
+
 import MyPageMain from 'pages/MyPageMain.jsx';
+import MyPageInfo from './pages/MyPageInfo.jsx';
 
 function App() {
   return (
@@ -23,14 +27,16 @@ function App() {
           <Route path="/" element={<AllBoardPage/>} />
           <Route path="/login" element={<Login/>} />
           <Route path="/signup" element={<SignUpPage/>} />
+          <Route path="/post" element={<EditerPage/>} />
+
           <Route path="/free" element={<FreeBoardPage/>} />
-          <Route path="/post/:postId/:userId" element={<FreeDetailPage />} />
-          <Route path="/mypage/info" element={<MyPageInfo/>} />
-          <Route path="/free/:postId/:userId" element={<FreeDetailPage />} />
+          <Route path="/free/:postId/:userId/:voteId" element={<FreeDetailPage />} />
           <Route path="/auth" element={<AuthBoardPage/>} />
           <Route path="/auth/:postId/:userId" element={<AuthDetailPage/>} />
           <Route path="/env" element={<EnvBoardPage/>} />
+
           <Route path="/mypage/main" element={<MyPageMain />} />
+          <Route path="/mypage/info" element={<MyPageInfo/>} />
         </Routes>
     </Router>
       {/* <FreeDetailPage/> */}
