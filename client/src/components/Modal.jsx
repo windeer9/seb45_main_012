0faa00f423da0b9b62fa011259f6d79f3db9deb1
matchaing <alswn202020@gmail.com>
2023-09-17 17:21:48 +0900

    chore : 주석 삭제

--- client/src/components/Modal.jsx
+++ client/src/components/Modal.jsx
@@ -5,7 +5,6 @@ import PropTypes from 'prop-types';
 
 const Modal = ({ content, onCancel, onConfirm }) => {
    return (
-   //   <div className='page_container'>
         <div className="modal_overlay">
             <div className="modal_container">
               <div className="modal_content">
@@ -21,7 +20,6 @@ const Modal = ({ content, onCancel, onConfirm }) => {
             </div>
             </div>
          </div>
-      // </div>
     );
   };
 
@@ -32,32 +30,3 @@ const Modal = ({ content, onCancel, onConfirm }) => {
   };
 
 export default Modal;
-
-
-
-//예시
-{/* <Modal
-  content={
-    <>
-      <p>탈퇴 안내</p>
-      <p>회원탈퇴를 신청하기 전에 안내 사항을 꼭 확인해주세요.</p>
-      <ul>
-        <li>✔ 사용하고 계신 아이디는 탈퇴 시 복구가 불가능합니다.</li>
-        <li>
-          ✔ 탈퇴 후 회원정보 및 개인형 서비스 이용기록은 모두 삭제됩니다.
-          <br />
-          &emsp;삭제된 데이터는 복구되지 않습니다. 삭제되는 내용을 확인하시고
-          <br />
-          &emsp;필요한 데이터는 미리 백업을 해주세요.
-        </li>
-        <li>
-          ✔ 탈퇴 후에도 게시판형 서비스에 등록한 게시물은 그대로 남아 있습니다.
-          <br />
-          &emsp;게시판형 서비스에 남아 있는 게시글은 탈퇴 후 삭제할 수 없습니다.
-        </li>
-      </ul>
-    </>
-  }
-  onCancel={handleCancel}
-  onConfirm={handleConfirm}
-/> */}
