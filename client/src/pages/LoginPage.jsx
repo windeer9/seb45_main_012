4fee18c94624ac98eac5e8c61bd5c8b65cf12039
jumpm9239 <jumpm9239@gmail.com>
2023-09-19 00:01:35 +0900

    feat: 로그인 에러 메세지 추가

--- client/src/pages/LoginPage.jsx
+++ client/src/pages/LoginPage.jsx
@@ -11,6 +11,7 @@ const LogIn = () => {
   
   const [ id, setId ] = useState("");
   const [ password, setPassword ] = useState("");
+  const [ errorMessage, setErrorMessage ] = useState("");
 
   const emailHandler = (e) => {
     setId(e.target.value);
@@ -22,46 +23,45 @@ const LogIn = () => {
 
   const submitHandler = async (e) => {
     e.preventDefault();
+    
     try {
       const result = await LoginFunc(id, password);
-      if (result) {
+      if (result === true) {
         dispatch(setActiveMenu('전체 글 보기'));
         navigate('/');
+      } else {
+        setErrorMessage(result);
       }
-    }
-    catch (err) {
+    } catch (err) {
       console.error()
     }
   }
 
   return (
-    <div >  
-      <div className='container'>  
-        <div className='logo_container'>
-          <img className="logo_leaf" src={require('../assets/logo_only_image.png')} alt='logo'/>
-        </div>
-        <div className='form_container'>
-          <form className='login_form' onSubmit={submitHandler}>
-              <div className='id'>
-                  <label htmlFor="id">ID</label>
-                  <input id="id" type="text" onChange={emailHandler}></input>
-              </div>
-              <div className='password'>
-                  <label htmlFor="password">Password</label>
-                  <input id="password" type="password" onChange={passwordHandler} />
-              </div>
-              <div className='submit_button'>
-                  <input className='submit' type="submit" value="로그인" />
-              </div>
-          </form>
+    <>  
+      <div className='login_container'>  
+        <img className="login_form_logo" src={require('../assets/logo_only_image.png')} alt='logo'/>
+        <form className='login_form' onSubmit={submitHandler}>
+          <div className='login_id'>
+            <label htmlFor="id">ID</label>
+            <input id="id" type="text" onChange={emailHandler}></input>
+          </div>
+          <div className='login_password'>
+            <label htmlFor="password">Password</label>
+            <input id="password" type="password" onChange={passwordHandler} />
+          </div>
+          <div className='error_message'>{errorMessage ? errorMessage : ''} </div>
+          <div className='submit_button'>
+            <input className='login_submit' type="submit" value="로그인" />
+          </div>
+        </form>
         </div>
         <div className='links'>
           <div>비밀번호 찾기 </div>
           <div className='center'> | </div>
           <Link to={'/signup'}>회원가입</Link>
         </div>
-      </div>
-    </div>
+    </>
   )
 }
 
