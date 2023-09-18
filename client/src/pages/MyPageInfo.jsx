c12346108f236a1f351ccf4b16888b79baf9ae7e
matchaing <alswn202020@gmail.com>
2023-09-18 16:50:18 +0900

    fix : 회원탈퇴 버튼

--- client/src/pages/MyPageInfo.jsx
+++ client/src/pages/MyPageInfo.jsx
@@ -157,37 +157,38 @@ const MyPageInfo = () => {
           </button>
               
         </div>
-        <button className='leave_button' onClick={handleOpenModal}>회원탈퇴</button>
-        {showModal && (
-          <Modal
-            content={
-              <>
-                <h3>탈퇴 안내</h3>
-                <h4>회원탈퇴를 신청하기 전에 안내 사항을 꼭 확인해주세요.</h4><br />
-                  <p>✔ 사용하고 계신 아이디는 탈퇴 시 복구가 불가능합니다.</p><br />
-                  <p>
-                    ✔ 탈퇴 후 회원정보 및 개인형 서비스 이용기록은 모두 삭제됩니다.
-                    <br />
-                    &emsp;삭제된 데이터는 복구되지 않습니다. 삭제되는 내용을 확인하시고
-                    <br />
-                    &emsp;필요한 데이터는 미리 백업을 해주세요.
-                  </p><br />
-                  <p>
-                    ✔ 탈퇴 후에도 게시판형 서비스에 등록한 게시물은 그대로 남아 있습니다.
-                    <br />
-                    &emsp;게시판형 서비스에 남아 있는 게시글은 탈퇴 후 삭제할 수 없습니다.
-                  </p><br />
-                  <h4>
-                    <input type="checkbox" checked={agreed} onChange={handleCheckboxChange} />
-                    안내 사항을 모두 확인하였으며, 이에 동의합니다.
-                  </h4>
-              </>
-            }
-            onCancel={handleCloseModal}
-            onConfirm={handleConfirm}
-          />
-        )}
-        
+        <div>
+          <button className='leave_button' onClick={handleOpenModal}>회원탈퇴</button>
+          {showModal && (
+            <Modal
+              content={
+                <>
+                  <h3>탈퇴 안내</h3>
+                  <h4>회원탈퇴를 신청하기 전에 안내 사항을 꼭 확인해주세요.</h4><br />
+                    <p>✔ 사용하고 계신 아이디는 탈퇴 시 복구가 불가능합니다.</p><br />
+                    <p>
+                      ✔ 탈퇴 후 회원정보 및 개인형 서비스 이용기록은 모두 삭제됩니다.
+                      <br />
+                      &emsp;삭제된 데이터는 복구되지 않습니다. 삭제되는 내용을 확인하시고
+                      <br />
+                      &emsp;필요한 데이터는 미리 백업을 해주세요.
+                    </p><br />
+                    <p>
+                      ✔ 탈퇴 후에도 게시판형 서비스에 등록한 게시물은 그대로 남아 있습니다.
+                      <br />
+                      &emsp;게시판형 서비스에 남아 있는 게시글은 탈퇴 후 삭제할 수 없습니다.
+                    </p><br />
+                    <h4>
+                      <input type="checkbox" checked={agreed} onChange={handleCheckboxChange} />
+                      안내 사항을 모두 확인하였으며, 이에 동의합니다.
+                    </h4>
+                </>
+              }
+              onCancel={handleCloseModal}
+              onConfirm={handleConfirm}
+            />
+          )}
+        </div>
       </div>
     </>
   );
