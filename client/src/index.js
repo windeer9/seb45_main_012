8b48cd963c3e8e55a420c8831da82ed1fe2f6f5a
jumpm9239 <jumpm9239@gmail.com>
2023-11-02 14:46:23 +0900

    chore: import 파일명 js -> ts로 변경

--- client/src/index.js
+++ client/src/index.js
@@ -4,7 +4,7 @@ import './index.css';
 import App from 'App.js';
 import { Provider } from 'react-redux';
 import { configureStore } from '@reduxjs/toolkit'; // configureStore를 import합니다.
-import menuSliceReducer from './store/menuSlice.js'; // 리듀서를 import합니다.
+import menuSliceReducer from './store/menuSlice.ts'; // 리듀서를 import합니다.
 
 
 // 스토어를 구성합니다.
